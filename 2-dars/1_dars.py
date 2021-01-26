36bb221ae35d7b7cf5e80c41622a6d9dc09f3837
Jalilovking <jalilovking@gmail.com>
2021-01-26 21:31:19 +0500

    Create 1_dars.py

---
+++ 2-dars/1_dars.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 13:42:54 2021
+
+@author: User
+"""
+
+# print('Assalom alaykum')
+
+# print('Hayrli tong!')
+
+# print(2+4*2)
+
+# print(19/5)
+
+# print(2**4)
